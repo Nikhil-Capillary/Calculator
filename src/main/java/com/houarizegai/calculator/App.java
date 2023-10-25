7d275e86bfaf60f1abd563fb06caf0e6c14ef456
Nikhil-Capillary <nikhil.sharma@capillarytech.com>
2023-10-25 19:03:38 +0530

    c8

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -7,4 +7,10 @@ public class App {
     public static void main(String[] args) {
         new CalculatorUI();
     }
+    public void stringEqualityCheckNoncompliant(String string1, String string2) {
+    // Noncompliant: the == operator doesn't compare the contents of the strings.
+    if(string1 == string2) {
+        System.out.println("The strings are equal.");
+    }
+
 }
