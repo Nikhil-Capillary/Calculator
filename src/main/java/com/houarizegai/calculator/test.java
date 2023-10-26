d06a361853b0deea80606d34f4421c3c6841f066
BONY MONDAL <bony.mondal@capillarytech.com>
2023-10-26 12:23:43 +0530

    fix

--- src/main/java/com/houarizegai/calculator/test.java
+++ src/main/java/com/houarizegai/calculator/test.java
@@ -1,8 +1,13 @@
-class HelloWorld {
-    public void createSqlConnectionNoncompliant() throws Exception {
-    // Noncompliant: password is hardcoded.
-    final Connection connection = DriverManager.getConnection("some url",
-            "username", "password");
-    connection.close();
-}
+public class App {
+     static void stringEqualityCheckNoncompliant(String string1, String string2) {
+    // Noncompliant: the == operator doesn't compare the contents of the strings.
+    if(string1 == string2) {
+        System.out.println("The strings are equal.");
+        }
+    }
+
+    public static void main(String[] args) {
+        stringEqualityCheckNoncompliant("dfhdh","dfhdh");
+    }
+
 }
