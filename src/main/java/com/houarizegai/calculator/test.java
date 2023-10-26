5364a6ddffcce259e15dc8bca58ee87a19ae8ae2
BONY MONDAL <bony.mondal@capillarytech.com>
2023-10-26 12:00:03 +0530

    fix

---
+++ src/main/java/com/houarizegai/calculator/test.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public void createSqlConnectionNoncompliant() throws Exception {
+    // Noncompliant: password is hardcoded.
+    final Connection connection = DriverManager.getConnection("some url",
+            "username", "password");
+    connection.close();
+}
+}
